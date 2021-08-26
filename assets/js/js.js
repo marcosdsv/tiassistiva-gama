f74b8bc24a44b708da5269465fbd2bccc5efde9f
marcosdsv <marcosbmx120@hotmail.com>
2021-08-26 16:56:16 -0300

    adicionei a função de máscara no telefone e o bloqueio de caracteres não numéricos

--- assets/js/js.js
+++ assets/js/js.js
@@ -12,3 +12,23 @@ function eventoCod(event){
 	}
 }
 
+function eventoTelefone(event){
+	//código dos digitos de 0 a 9 48 57.
+var cod = event.which || event.keycode;
+var telefone = document.getElementById("telefone").value;
+if(cod >= 65 && cod <= 90 || cod >= 97 && cod <= 122){
+	event.preventDefault();
+}
+if(telefone.length == 0 && cod >= 48 && cod <= 57){
+telefone = telefone + '(';
+document.forms[0].telefone.value = telefone;
+}
+if(telefone.length == 3){
+telefone = telefone + ')';
+document.forms[0].telefone.value = telefone;
+}
+if(telefone.length == 8){
+telefone = telefone + '-';
+document.forms[0].telefone.value = telefone;
+}
+}
