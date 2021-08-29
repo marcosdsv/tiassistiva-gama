48794e4947c80726e0130ac625539f77e0369beb
marcosdsv <marcosbmx120@hotmail.com>
2021-08-28 22:34:35 -0300

    realizei comentários no código

--- assets/js/validaCampo.js
+++ assets/js/validaCampo.js
@@ -1,4 +1,6 @@
+//Função é executada ao ser acionada pelo botão.
 function proximo1(){
+	//declaração de variáveis.
 var nome = document.getElementById("nome").value;
 var telefone = document.getElementById("telefone").value;
 var celular = document.getElementById("celular").value;
@@ -9,69 +11,87 @@ var sexo = document.getElementById("sexo").value;
 var resultadoTelefone1 = telefone.substring(0, 1);
 var resultadoTelefone2 = telefone.substring(3, 4);
 var resultadoTelefone3 = telefone.substring(8, 9);
+
 //pego os caracteres da máscara do celular.
 var resultadoCelular1 = celular.substring(0, 1);
 var resultadoCelular2 = celular.substring(3, 4);
 var resultadoCelular3 = celular.substring(9, 10);
+
 // pega os caracteres da máscara do cpf
 var resultadoCpf1 = cpf.substring(3, 4);
 var resultadoCpf2 = cpf.substring(7, 8);
 var resultadoCpf3 = cpf.substring(11, 12);
-
+//Verifica se o campo nome atende todos os argumentos.
 if(nome.length <= 5){
 	document.getElementById("nome").focus();
 }
+//Verifica se o campo telefone atende todos os argumentos.
 else if(!(!(!(resultadoTelefone1 == "(") && resultadoTelefone2 == ")") && resultadoTelefone3 == "-") || telefone.length <= 12){
     document.getElementById("telefone").focus();
 }
+//Verifica se o campo celular atende todos os argumentos.
 else if(!(!(!(resultadoCelular1 == "(") && resultadoCelular2 == ")") && resultadoCelular3 == "-") || celular.length <= 13){
     document.getElementById("celular").focus();
 }
+//Verifica se o campo CPF atende todos os argumentos.
 else if(!(!(!(resultadoCpf1 == ".") && resultadoCpf2 == ".") && resultadoCpf3 == "-") || cpf.length <= 13){
     document.getElementById("cpf").focus();
 }
+//Verifica se está em branco.
 else if(rg == ""){
 	document.getElementById("rg").focus();
 }
+//Verifica se o campo sexo está em branco.
 else if(sexo == ""){
 	document.getElementById("sexo").focus();
 }else{
+	//Oculta a div1 e mostra a div2.
     document.getElementById("div1").style.display = 'none';
     document.getElementById("div2").style.display = 'block';
 	document.getElementById("cep").focus();
 }
 
 }
-
+//Função é executada ao ser acionada pelo botão.
 function proximo2(){
+	//Declaração de variáveis.
 	var cep = document.getElementById("cep").value;
 	var endereco = document.getElementById("endereco").value;
 	var bairro = document.getElementById("bairro").value;
 	var numero = document.getElementById("numero").value;
+	//Verifica se o campo CEP está em branco.
 	if(cep == ""){
 		document.getElementById("cep").focus();
 	}
+	//Verifica se o campo endereço está em branco.
 	else if(endereco == ""){
 		document.getElementById("endereco").focus();
 	}
+	//Verifica se o campo Bairro está em branco.
 	else if(bairro == ""){
 		document.getElementById("bairro").focus();
 	}
+	//Verifica se o campo cidade está em branco.
 	else if(cidade == ""){
 		document.getElementById("cidade").focus();
 	}
+	//Verifica se o campo estado está em branco.
 	else if(estado == ""){
 		document.getElementById("estado").focus();
 	}
+	//Verifica se o campo número está em branco.
 	else if(numero == ""){
 		document.getElementById("numero").focus();
 	}else{
+		////Oculta a div2 e mostra a div3.
 		document.getElementById("div2").style.display = 'none';
 		document.getElementById("div3").style.display = 'block';
 		document.getElementById("visual").focus();
 	}
 }
+//Função é executada ao ser acionada pelo botão.
 function proximo3(){
+	//Declaração de variáveis.
 var visual = document.getElementById("visual").checked;
 var surdo = document.getElementById("surdo").checked;
 var fisica = document.getElementById("fisica").checked;
@@ -79,46 +99,58 @@ var intelectual = document.getElementById("intelectual").checked;
 var php = document.getElementById("php").checked;
 var java = document.getElementById("java").checked;
 var python = document.getElementById("python").checked;
+//Verifica se o radio está marcado.
 if(visual != true && surdo != true && fisica != true &&intelectual != true){
 alert("Pelo menos deve selecionar uma deficiência!");
 document.getElementById("visual").focus();
 }
+//Verifica se algum checkbox está marcado.
 else if(php != true && java != true && python != true){
 alert("Selecione pelo menos um curso!");
 document.getElementById("php").focus();
 }else{
+	//Oculta a div3 e mostra a div4
 	document.getElementById("div4").style.display = 'block';
 	document.getElementById("div3").style.display = 'none';
 	document.getElementById("email").focus();
 }
 }
+//Função é executada ao ser acionada pelo botão.
 function proximo4(){
+	//declaração de variáveis.
 	var email = document.getElementById("email").value;
 	var senha1 = document.getElementById("senha1").value;
 	var senha2 = document.getElementById("senha2").value;
+	//Verifica se o campo e-mail está em branco.
 	if(email == ""){
 		document.getElementById("email").focus();
 	}
+	//Verifica se o primeiro input de senha está em branco.
 	else if(senha1 == ""){
 		alert("O campo senha está em branco");
 		document.getElementById("senha1").focus();
 	}
+	//Verifica se o segundo input de senha está em branco.
 	else if(senha2 == ""){
 		alert("O campo senha está em branco");
 		document.getElementById("senha2").focus();
 	}
+	//Verifica se a primeira senha é diferente da segunda.
 	else if(senha1 != senha2){
 		alert("as senhas não confere! Digite novamente");
 		document.getElementById("senha1").focus();
 	}
+	//Verifica se a primeira senha é menor que 8.
 	else if(senha1.length < 8){
 		alert("a senha deve ter pelo menos 8 caracteres");
 		document.getElementById("senha1").focus();
 	}
+	//Verifica se a segunda senha é menor que 8.
 	else if(senha2.length < 8){
 		alert("a senha deve ter pelo menos 8 caracteres");
 		document.getElementById("senha2").focus();
 	}else{
+		//mostra um alert com dois botões.
 		if(confirm("tem certeza que deseja se cadastrar?")){
 	window.location="cadastrado.html";
 		}
