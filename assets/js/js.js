1a129d5273229db82cb2ce67ae9270968ad95e24
marcosdsv <marcosbmx120@hotmail.com>
2021-08-28 22:35:57 -0300

    adicionei comentários no código

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,15 +1,18 @@
+//Função para verificar a tecla digitada.
 function eventoCod(event){
+	//Declaração de variáveis.
 	var cod = event.which || event.keycode;
 	var nome = document.getElementById("nome").value;
 	//if(cod >= 65 && cod <= 90 || cod >= 97 && cod <= 122){
+		//Verifica se a tecla digitada está de acordo.
 		if(cod >= 48 && cod <= 57){
 			event.preventDefault();
 	}
-	if(nome.length <= 5){
-		document.getElementById("divnome").innerHTML = "Digite seu nome completo";
+	/*if(nome.length <= 5){
+		document.getElementById("pnome").innerHTML = "Digite seu nome completo";
 	}else{
-		document.getElementById("divnome").innerHTML = "";
-	}
+		document.getElementById("pnome").innerHTML = "";
+	}*/
 }
 
 function eventoTelefone(event){
